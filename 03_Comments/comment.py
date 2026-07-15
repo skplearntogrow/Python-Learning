cde5dc1e0a548cc33ccbb8df0292c816be4f0766
Santosh Kumar Potharaju <skplearntogrow@gmail.com>
2026-07-15 21:30:46 +0530

    Saved file

---
+++ 03_Comments/comment.py
@@ -0,0 +1,9 @@
+# This is a comment
+print("This is a print statement") #This is an inline comment
+#print('Am learning Python,it is fun')
+#This is multi line comment 1
+#This is multi line comment 2
+"""
+This is a multiline string comment
+for learning purpose
+.Its fun to learn python programming language"""

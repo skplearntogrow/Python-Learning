ddf5972e5c1f9625894392c901e98b319413e7bc
Santosh Kumar Potharaju <skplearntogrow@gmail.com>
2026-07-16 20:29:29 +0530

    Learnt and practiced Random numbers creation using python built-in module random by importing module into the code.

---
+++ 06_Numbers/Random_Numbers.py
@@ -0,0 +1,9 @@
+# In this chapter we learn, write and explain creating Random numbers in Python using random module.
+# Module is a .py file
+"""Python does not have Random function to make Random numbers but python has a built-in module called random
+that can be used to make random numbers"""
+# Import random module to display random numbers from 1 to 9
+import random
+print(random.randrange(1,10))
+# random.randrange(start,stop,step)
+print(random.randrange(1,5))

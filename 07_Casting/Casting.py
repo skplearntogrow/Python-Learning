921a6f7a55172e9576c88213d6a29411b23956bb
Santosh Kumar Potharaju <skplearntogrow@gmail.com>
2026-07-20 21:55:11 +0530

    Casting Chapter learning, coding examples practice

---
+++ 07_Casting/Casting.py
@@ -0,0 +1,29 @@
+# Casting to specify a type to a variable.
+# Casting is performed in python using Constructor built-in functions
+x= int(1)
+y= int(2.8)
+z= int("3")
+print(x,end=",")
+print(y,end=",")
+print(z)
+a= float(1)
+b= float(2.8)
+c= float("3")
+d= float("4.2")
+print(a,end=",")
+print(b,end=",")
+print(c,end=",")
+print(d)
+x= str("s1")
+y= str(2)
+z= str(3.0)
+print(x,end=",")
+print(y,end=",")
+print(z)
+# Numeric value can be converted to string, a float value can be converted to string; though value will still be numeric and float values.
+# Coding Challenge
+x= 1
+a= float(x)
+b= str(x)
+print(a,end=",")
+print(b)
---
+++ 07_Casting/notes.md
@@ -0,0 +1,4 @@
+**Topics Covered:**
+1. Python Casting
+2. Casting of integer, float and string variables.
+3. Type conversion code examples

b8a96056d7ee8c832e772ae53b8df99c9614f323
Santosh Kumar Potharaju <skplearntogrow@gmail.com>
2026-07-23 12:36:44 +0530

    Python None constant learning and Practicing

---
+++ 10_None/none.py
@@ -0,0 +1,22 @@
+# Python None constant
+x= None
+print(x)
+print(type(x))
+# Comparing to None using identity operators is or is not
+result= None
+if result is None:
+    print("There is No Result.All the Best.")
+else: print("You got the Result.")
+area= None
+if area is not None:
+    print("You know the location.")
+else: print("You dont know the location.")
+# None evaluates to False in boolean context
+print(bool(area))
+print(bool(None))
+# Function returning None. Functions that do not explicitly return a value return None.
+def myfun():
+    x=5
+    return 5
+x= myfun()
+print(x)
